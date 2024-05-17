3f6a76b833a22fcefc6500ef0bcdb04e0bd760ff
Michał Dobaczewski <mdobak@gmail.com>
2024-05-17 16:05:54 +0200

    Make Gofer config backward compatible with 0.36 [sc-5795] (#168)
    Co-authored-by: Chronicle Protocol <devops@chroniclelabs.org>

--- hcl/funcs/semver.go
+++ hcl/funcs/semver.go
@@ -1,8 +1,24 @@
+//  Copyright (C) 2021-2023 Chronicle Labs, Inc.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package funcs
 
 import (
+	"errors"
 	"fmt"
-	"unicode"
+	"strings"
 
 	"github.com/zclconf/go-cty/cty"
 	"github.com/zclconf/go-cty/cty/function"
@@ -10,19 +26,12 @@ import (
 )
 
 const (
-	semverLess         byte = '<'
-	semverEqual        byte = '='
-	semverGreaterEqual byte = '~'
-	semverGreater      byte = '>'
-)
-
-var (
-	semverCompChars = []byte{
-		semverLess,
-		semverEqual,
-		semverGreaterEqual,
-		semverGreater,
-	}
+	semverLess         string = "<"
+	semverLessEqual    string = "<="
+	semverGreater      string = ">"
+	semverGreaterEqual string = ">="
+	semverEqual        string = "="
+	semverNotEqual     string = "!="
 )
 
 func Semver() function.Function {
@@ -30,104 +39,133 @@ func Semver() function.Function {
 		Description: "Checks if semver matches target",
 		Params: []function.Parameter{
 			{
-				Name:         "version",
-				Description:  "semver to check",
-				Type:         cty.String,
-				AllowNull:    false,
-				AllowUnknown: false,
-				// TODO: not sure about marked values, maybe they should be allowed
-				AllowMarked:      false,
+				Name:             "version",
+				Description:      "Semver version to check.",
+				Type:             cty.String,
+				AllowNull:        false,
+				AllowUnknown:     false,
+				AllowMarked:      true,
 				AllowDynamicType: true,
 			},
 			{
-				Name:         "target",
-				Description:  "conditional target to check",
-				Type:         cty.String,
-				AllowNull:    false,
-				AllowUnknown: false,
-				// TODO: not sure about marked values, maybe they should be allowed
-				AllowMarked:      false,
+				Name:             "target",
+				Description:      "Semver target to check against.",
+				Type:             cty.String,
+				AllowNull:        false,
+				AllowUnknown:     false,
+				AllowMarked:      true,
 				AllowDynamicType: true,
 			},
 		},
 		Type: func(args []cty.Value) (cty.Type, error) {
 			if len(args) != 2 {
-				return cty.NilType, fmt.Errorf("expected 2 arguments")
+				return cty.NilType, errors.New("expected 2 arguments")
 			}
 			if !args[0].Type().Equals(cty.String) {
-				return cty.NilType, fmt.Errorf("version should be a string")
+				return cty.NilType, errors.New("version should be a string")
 			}
 			if !args[1].Type().Equals(cty.String) {
-				return cty.NilType, fmt.Errorf("target should be a string")
+				return cty.NilType, errors.New("target should be a string")
 			}
 			return cty.Bool, nil
 		},
 		Impl: func(args []cty.Value, refType cty.Type) (cty.Value, error) {
 			if refType != cty.Bool {
-				return cty.NilVal, fmt.Errorf("invalid arguments")
+				return cty.NilVal, errors.New("invalid arguments")
 			}
 
-			ver := args[0].AsString()
+			version := args[0].AsString()
 			target := args[1].AsString()
 
+			if len(version) == 0 {
+				return cty.BoolVal(false), errors.New(`invalid version`)
+			}
 			if len(target) == 0 {
-				return cty.BoolVal(false), fmt.Errorf(`invalid target ""`)
+				return cty.BoolVal(false), errors.New(`invalid target`)
 			}
 
-			cond := semverEqual
-			if !unicode.IsDigit(rune(target[0])) {
-				found := false
-				for _, c := range semverCompChars {
-					if c == target[0] {
-						cond = c
-						found = true
-						target = target[1:]
-						break
-					}
-				}
-				if !found {
-					return cty.BoolVal(false), fmt.Errorf("invalid target %q: unknown condition", target)
+			condition := semverEqual
+			switch {
+			// 2 char conditions
+			case strings.HasPrefix(target, semverLessEqual):
+				condition = semverLessEqual
+			case strings.HasPrefix(target, semverGreaterEqual):
+				condition = semverGreaterEqual
+			case strings.HasPrefix(target, semverNotEqual):
+				condition = semverNotEqual
+			// 1 char conditions
+			case strings.HasPrefix(target, semverLess):
+				condition = semverLess
+			case strings.HasPrefix(target, semverGreater):
+				condition = semverGreater
+			case strings.HasPrefix(target, semverEqual):
+				condition = semverEqual
+			default:
+			}
+
+			// Special case, dev version.
+			// The dev version is always higher than any other version.
+			if version == "dev" {
+				switch condition {
+				case semverLess:
+					return cty.False, nil
+				case semverLessEqual:
+					return cty.False, nil
+				case semverGreater:
+					return cty.True, nil
+				case semverGreaterEqual:
+					return cty.True, nil
+				case semverEqual:
+					return cty.False, nil
+				case semverNotEqual:
+					return cty.True, nil
 				}
 			}
 
-			ver = "v" + ver
-			target = "v" + target
+			target = strings.TrimPrefix(target, condition)   // remove condition from target
+			version = "v" + strings.TrimPrefix(version, "v") // add v prefix to version if missing
+			target = "v" + strings.TrimPrefix(target, "v")   // add v prefix to target if missing
 
-			if !semver.IsValid(ver) {
-				return cty.BoolVal(false), fmt.Errorf("invalid version %q", ver)
+			if !semver.IsValid(version) {
+				return cty.BoolVal(false), fmt.Errorf("invalid version %q", version[1:])
 			}
 			if !semver.IsValid(target) {
-				return cty.BoolVal(false), fmt.Errorf("invalid target %q", target)
+				return cty.BoolVal(false), fmt.Errorf("invalid target %q", target[1:])
 			}
 
-			switch semver.Compare(ver, target) {
+			switch semver.Compare(version, target) {
 			case -1:
-				switch cond {
+				switch condition {
 				case semverLess:
-					return cty.BoolVal(true), nil
+					return cty.True, nil
+				case semverLessEqual:
+					return cty.True, nil
 				default:
-					return cty.BoolVal(false), nil
+					return cty.False, nil
 				}
 			case 0:
-				switch cond {
-				case semverEqual:
-					return cty.BoolVal(true), nil
+				switch condition {
+				case semverLessEqual:
+					return cty.True, nil
 				case semverGreaterEqual:
-					return cty.BoolVal(true), nil
+					return cty.True, nil
+				case semverEqual:
+					return cty.True, nil
 				default:
-					return cty.BoolVal(false), nil
+					return cty.False, nil
 				}
 			case 1:
-				switch cond {
+				switch condition {
 				case semverGreater:
-					return cty.BoolVal(true), nil
+					return cty.True, nil
 				case semverGreaterEqual:
-					return cty.BoolVal(true), nil
+					return cty.True, nil
 				default:
-					return cty.BoolVal(false), nil
+					return cty.False, nil
 				}
 			}
-			panic("should be unreachable")
+
+			panic("unreachable")
 		},
 	}
 	return function.New(&spec)
